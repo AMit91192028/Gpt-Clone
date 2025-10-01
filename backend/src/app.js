608c4d838976adc007a49af45c1f38fe04e523b6
AMit91192028 <amit3192352@gmail.com>
2025-10-01 13:18:40 +0530

    backend url changes on frontend done

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,7 @@ const cookieParser = require('cookie-parser')
 const authRouter = require('../src/router/auth.routes')
 const chatRouter = require('../src/router/chat.routes')
 const cors = require('cors');
-
+const path = require('path')
 const app = express();
 app.use(cors({
     origin:'http://localhost:5173',
@@ -11,12 +11,15 @@ app.use(cors({
 }))
 app.use(express.json())
 app.use(cookieParser())
-
+app.use(express.static(path.join(__dirname,'../public')))
 
 
 app.use('/api/auth',authRouter);
 app.use('/chat',chatRouter);
 
+app.get('*name',(req,res)=>{
+    res.sendFile(path.join(__dirname,'../public/index.html'))
+})
 
 
 module.exports= app
