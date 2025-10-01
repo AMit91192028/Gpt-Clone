b115207b0b226d1d2d6cba004cf7605649be96ad
AMit91192028 <amit3192352@gmail.com>
2025-10-01 15:38:53 +0530

    updating with npm run build

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,9 @@
-import React, { useEffect, useState } from 'react'
+
 import AppRoutes from './AppRoutes'
-import axios from 'axios';
+
 
 const App = () => {
-  //   const [auth, setAuth] = useState({ loading: true, user: null });
 
-  // useEffect(() => {
-  //   axios.get("http://localhost:3000/api/auth/check", { withCredentials: true })
-  //     .then(res => setAuth({ loading: false, user: res.data.user }))
-  //     .catch(() => setAuth({ loading: false, user: null }));
-  // }, [auth]);
   return (
     <div>
       <AppRoutes/>
